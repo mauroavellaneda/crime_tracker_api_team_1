3b98c9cf10c95b40cecaacec2cbacb15670d7775
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-10-03 18:53:16 +0200

    Adds omniauth_callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'api/v1/auth'
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
   namespace :api, constraints: { format: 'json'} do
     namespace :v1 do
       resources :reports, only: [:index]
